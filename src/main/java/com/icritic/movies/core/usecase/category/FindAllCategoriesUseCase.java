db88d8af2d02b42570a88b7d9d95570f506bcd60
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-11-27 19:31:10 -0300

    Usecases and boundarys for find all categories endpoint

---
+++ src/main/java/com/icritic/movies/core/usecase/category/FindAllCategoriesUseCase.java
@@ -0,0 +1,22 @@
+package com.icritic.movies.core.usecase.category;
+
+import com.icritic.movies.core.model.Category;
+import com.icritic.movies.core.usecase.boundary.FindAllCategoriesBoundary;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class FindAllCategoriesUseCase {
+
+    private final FindAllCategoriesBoundary findAllCategoriesBoundary;
+
+    public List<Category> execute() {
+        log.info("Finding all categories");
+        return findAllCategoriesBoundary.execute();
+    }
+}
--- src/main/java/com/icritic/movies/dataprovider/database/repository/CategoryEntityRepository.java
+++ src/main/java/com/icritic/movies/dataprovider/database/repository/CategoryEntityRepository.java
@@ -3,7 +3,11 @@ package com.icritic.movies.dataprovider.database.repository;
 import com.icritic.movies.dataprovider.database.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryEntityRepository extends JpaRepository<CategoryEntity, Long> {
 
     CategoryEntity findByName(String name);
+
+    List<CategoryEntity> findAllByOrderByIdAsc();
 }
