42cddb67368e0dc188b5aa3b503764e4e9f0152f
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-12-09 15:34:28 -0300

    Adding tests for mappers

---
+++ src/test/java/com/icritic/movies/entrypoint/mapper/ActorDtoMapperTest.java
@@ -0,0 +1,24 @@
+package com.icritic.movies.entrypoint.mapper;
+
+import com.icritic.movies.core.model.Actor;
+import com.icritic.movies.core.usecase.fixture.ActorFixture;
+import com.icritic.movies.entrypoint.dto.actor.ActorResponseDto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ActorDtoMapperTest {
+
+    @Test
+    void givenActor_thenReturnActorResponseDto() {
+        Actor actor = ActorFixture.load();
+
+        ActorResponseDto actorResponseDto = ActorDtoMapper.INSTANCE.actorToActorResponseDto(actor);
+
+        assertThat(actorResponseDto).isNotNull();
+        assertThat(actorResponseDto.getId()).isEqualTo(actor.getId());
+        assertThat(actorResponseDto.getName()).isEqualTo(actor.getName());
+        assertThat(actorResponseDto.getDescription()).isEqualTo(actor.getDescription());
+        assertThat(actorResponseDto.getCountryId()).isEqualTo(actor.getCountry().getId());
+    }
+}
